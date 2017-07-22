771b3bc6bcae660e2d4a9322ca12f2e54bed34b5
Afonso Muralha <afonsomuralha@gmail.com>
2017-07-22 21:03:16 +0100

    added arduino code

--- Arduino/SlideyArduino/SlideyArduino.ino
+++ Arduino/SlideyArduino/SlideyArduino.ino
@@ -8,16 +8,29 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  pinMode(pot, INPUT);
+  pinMode(INA, OUTPUT);
+  pinMode(INB, OUTPUT);
 	Serial.begin(9600);
-	sendData();
+	//sendData();
+  savePos();
 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+  if(Serial.available()>0){
+    int x = Serial.parseInt();
+    if(x != -1 || x != 0){
+      Serial.print("Going to "); Serial.println(x);
+      setPos(x);
+    }
+    else{
+      currentPos();
+    }
+  }
 }
 
 
-void sendData();
+//void sendData();
 
--- Arduino/SlideyArduino/funcsanddefs.h
+++ Arduino/SlideyArduino/funcsanddefs.h
@@ -1,26 +1,79 @@
 #pragma once
 
+#define INA 10
+#define INB 9
+#define pot A0
+
+#define defspeed 250
+
+int lastPos;
 
 
 
-void sendData() {
-	int x = 1;
-	String y = "S1";
-	
-	while (true)
-	{
-		y = "S1" + String(x);
-		Serial.println(y);
-		y = "";
-		if (x == 100) {
-			x = 1;
-		}
-		else
-		{
-			x++;
-		}
-		delay(500);
-	}
-	
-	
+void sendData(String out) {
+		Serial.println(out);
 }
+
+void runMotor(char dir, int speed){
+  switch (dir){
+    case 'u':
+      analogWrite(INA, speed);
+      analogWrite(INB, 0);
+      break;
+    case 'd':
+      analogWrite(INB, speed);
+      analogWrite(INA, 0);
+     break;
+    case 's':
+      analogWrite(INA, 0);
+      analogWrite(INB, 0);
+      break;
+    default:
+    break;
+  }
+}
+
+void stopMotor(){
+  runMotor('s', 0);
+}
+
+int readPot(){
+  int read = analogRead(pot);
+  int conv = map(read, 0, 1024, 0, 100);
+  return conv;
+}
+
+void savePos(){
+  lastPos = readPot();
+  Serial.print("Saved pos "); Serial.println(lastPos);
+}
+void currentPos(){
+  int current = readPot();
+  Serial.print("Current pos "); Serial.println(current);
+}
+
+void setPos(int pos){
+  //int conv = map(pos, 0, 100, 0, 1024);
+  int read = readPot();
+  while(readPot() != pos){
+    
+    Serial.print("Current pos "); Serial.println(read);
+    read = readPot();
+    
+    if(pos > read){
+      //tem de subir
+      runMotor('u', defspeed);
+    }
+    else if(pos<read){
+      //tem de descer
+      runMotor('d', defspeed);
+    }
+    else{
+      stopMotor();
+    }
+  }
+  stopMotor();
+  savePos();
+  
+}
+
